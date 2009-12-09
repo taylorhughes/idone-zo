ed7472c2ebe4c791a7a15edf7933e50438989e97
tdawg <tdawg@21c2b8b0-c4d0-461e-9b94-4eb597c105ab>
2009-12-09 01:20:21 +0000

    Make RevisionInfo.h.rb, which contains the (ruby) code to do the same thing we were doing before with the shell script. This also properly sorts integers. Use this for both targets.
    git-svn-id: svn+ssh://taylor-hughes.com/svnroot/idnzo/trunk@187 21c2b8b0-c4d0-461e-9b94-4eb597c105ab

---
+++ idnzo/RevisionInfo.h.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+# RevisionInfo.h.rb
+# DNZO
+#
+# Created by Taylor Hughes on 12/8/09.
+# Copyright 2009 Two-Stitch Software. All rights reserved.
+
+# Creates $DSTROOT/RevisionInfo.h, which is included in the build and defines SVN_REVISION
+
+require 'fileutils'
+
+def main
+  revs = `svn info #{ENV['SRCROOT']} -R |grep Revision`
+  revision = revs.collect do |line|
+    line.gsub(/[^\d]+/,'').to_i
+  end.max
+  FileUtils.mkdir_p(ENV['DERIVED_FILES_DIR'])
+  File.open(File.join(ENV['DERIVED_FILES_DIR'], 'RevisionInfo.h'), 'w') do |file|
+    file.puts '#define SVN_REVISION @"%d"' % revision
+  end
+end
+
+main if $0 == __FILE__
